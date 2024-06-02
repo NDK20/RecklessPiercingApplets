0782de723ae19394709febbf580a0f6ae928fec4
NDK20 <ndk3@usf.edu>
2024-06-02 17:43:26 +0000

    PersonalInfo
    Module 1 Exercise 1.1

--- main.cs
+++ main.cs
@@ -2,6 +2,9 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.WriteLine("Elizabeth France");
+    Console.WriteLine("1/12/1999");
+    Console.WriteLine("work 850-848-9928");
+    Console.WriteLine("cell 637-889-3833");
   }
 }
