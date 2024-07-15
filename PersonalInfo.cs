93ef339898f3fc71b6685c7a8c8d4172cd089f33
NDK20 <ndk3@usf.edu>
2024-07-15 23:14:18 +0000

    InchesToCentimeters

--- PersonalInfo.cs
+++
@@ -1,9 +0,0 @@
-using System;
-class MainClass {
-  public static void Main (string[] args) {
-    Console.WriteLine("Lucinda Potter");
-    Console.WriteLine("6/24/1992");
-    Console.WriteLine("work 000-000-0101");
-    Console.WriteLine("cell 000-000-0189");
-  }
-}
--- main.cs
+++ main.cs
@@ -1,10 +1,20 @@
 using System;
 
 class Program {
+  // number of centimeters in an inch
+  const double CentimetersPerInch = 2.54;
   public static void Main (string[] args) {
-    Console.WriteLine("Lucinda Potter");
-    Console.WriteLine("6/24/1992");
-    Console.WriteLine("work 000-000-0101");
-    Console.WriteLine("cell 000-000-0189");
+    //input
+    double inches = 5;
+    double y = 50;
+    double z = -15;
+    double a = 15994;
+    //conversion
+    double centimeters = inches * 2.54;
+    //result
+    Console.WriteLine($"{inches} inches is {inches * CentimetersPerInch} centimeters");
+    Console.WriteLine($"{y} inches is {y * CentimetersPerInch} centimeters");
+    Console.WriteLine($"{z} inches is {z * CentimetersPerInch} centimeters");
+    Console.WriteLine($"{a} inches is {a * CentimetersPerInch} centimeters");
   }
 }
