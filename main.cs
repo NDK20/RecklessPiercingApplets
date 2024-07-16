ce3c5fad0a1a1af21b695ac0974087f1b9810135
NDK20 <ndk3@usf.edu>
2024-07-16 06:18:04 +0000

    Module 2: Exercise 2.2 (MoveEstimator)

--- main.cs
+++ main.cs
@@ -1,24 +1,25 @@
 using System;
-
-class Program {
-  // number of centimeters in an inch
-  const double CentimetersPerInch = 2.54;
+  class Program {
   public static void Main (string[] args) {
-
-    //prompt for input 
-    Console.WriteLine("Enter a number of inches: ");
-
-    //read input and convert to double 
-    if (double.TryParse(Console.ReadLine(), out double inches)) {
-    
-    //calc centimeters
-    double centimeters = inches * CentimetersPerInch;
-    
+    //constants for rates
+    const double BASE_RATE = 200;
+    const double HOURLY_RATE = 150;
+    const double MILES_RATE = 2;
+    //prompt for input
+    //number of hours worked
+    Console.WriteLine("Enter the number of hours worked: ");
+    string hours = Console.ReadLine();
+    //number of miles driven
+    Console.WriteLine("Enter the number of miles driven: ");
+    string miles = Console.ReadLine();
+    //parse input to double
+    if (double.TryParse(hours, out double hoursworked) && double.TryParse(miles, out double milesdriven)) {
+    //calc total
+    double total = BASE_RATE + (HOURLY_RATE * hoursworked) + (MILES_RATE * milesdriven);
     //output result
-    Console.WriteLine("{0} inches is {1} centimeters", inches, centimeters);
-
-    //invalid input
-      } else {
+    Console.WriteLine("The total cost of the trip is: ${0}", total);
+    } else {
+      //invalid input}  
       Console.WriteLine("Invalid input. Please enter a number.");
     }
   }
