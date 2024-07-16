ca3d6552a055ae2bed0c9d55dc58ca03c3a9b135
NDK20 <ndk3@usf.edu>
2024-07-16 03:21:46 +0000

    Module 2: Exercise 2.1.1 (InchesToCentimetersInteractive)

--- main.cs
+++ main.cs
@@ -4,17 +4,22 @@ class Program {
   // number of centimeters in an inch
   const double CentimetersPerInch = 2.54;
   public static void Main (string[] args) {
-    //input
-    double inches = 5;
-    double y = 50;
-    double z = -15;
-    double a = 15994;
-    //conversion
-    double centimeters = inches * 2.54;
-    //result
-    Console.WriteLine($"{inches} inches is {inches * CentimetersPerInch} centimeters");
-    Console.WriteLine($"{y} inches is {y * CentimetersPerInch} centimeters");
-    Console.WriteLine($"{z} inches is {z * CentimetersPerInch} centimeters");
-    Console.WriteLine($"{a} inches is {a * CentimetersPerInch} centimeters");
+
+    //prompt for input 
+    Console.WriteLine("Enter a number of inches: ");
+
+    //read input and convert to double 
+    if (double.TryParse(Console.ReadLine(), out double inches)) {
+    
+    //calc centimeters
+    double centimeters = inches * CentimetersPerInch;
+    
+    //output result
+    Console.WriteLine("{0} inches is {1} centimeters", inches, centimeters);
+
+    //invalid input
+      } else {
+      Console.WriteLine("Invalid input. Please enter a number.");
+    }
   }
 }
