a416a465a3ddf86cbe584aee4ac908eecb6e2065
NDK20 <ndk3@usf.edu>
2024-07-17 21:53:34 +0000

    Module 3 Exercise 3.1 (Twitter)

--- main.cs
+++ main.cs
@@ -1,26 +1,13 @@
 using System;
-  class Program {
+
+class Program {
   public static void Main (string[] args) {
-    //constants for rates
-    const double BASE_RATE = 200;
-    const double HOURLY_RATE = 150;
-    const double MILES_RATE = 2;
-    //prompt for input
-    //number of hours worked
-    Console.WriteLine("Enter the number of hours worked: ");
-    string hours = Console.ReadLine();
-    //number of miles driven
-    Console.WriteLine("Enter the number of miles driven: ");
-    string miles = Console.ReadLine();
-    //parse input to double
-    if (double.TryParse(hours, out double hoursworked) && double.TryParse(miles, out double milesdriven)) {
-    //calc total
-    double total = BASE_RATE + (HOURLY_RATE * hoursworked) + (MILES_RATE * milesdriven);
-    //output result
-    Console.WriteLine("The total cost of the trip is: ${0}", total);
+    Console.WriteLine ("Enter message to be encrypted:");
+    string message = Console.ReadLine();
+    if(message.Length <= 140) {
+      Console.WriteLine("Posted");
     } else {
-      //invalid input}  
-      Console.WriteLine("Invalid input. Please enter a number.");
+      Console.WriteLine("Rejected");
     }
   }
 }
